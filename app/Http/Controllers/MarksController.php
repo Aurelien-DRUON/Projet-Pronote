f127ddc75c8adaeb4af674018a2a206fa26fb4d8
Aurelien-DRUON <aurelien.druon@epfedu.fr>
2024-02-14 11:01:02 +0100

    faites pas attention monsieur tout est normal

--- app/Http/Controllers/MarksController.php
+++ app/Http/Controllers/MarksController.php
@@ -2,9 +2,26 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Mark;
 use Illuminate\Http\Request;
 
 class MarksController extends Controller
 {
-    //
+    public function create(Request $request)
+    {
+        $mark = new Mark();
+        $mark->student_id = auth()->user()->id;
+        $mark->user_id = auth()->user()->id;
+        $mark->mark = $request->input('mark');
+        $mark->description = $request->input('description');
+        $mark->date = $request->input('date');
+        $mark->save();
+
+        return redirect()->route('marks.read');
+    }
+    public function read()
+    {
+        $marks = Mark::find(1);
+        return view('marks.read', ['marks' => $marks]);
+    }
 }
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,3 +1,7 @@
+<?php
+dd(Hash::make('password'));
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
