7e8b141b6f7cba9059d94173569bcf8f15d8a4a3
Aurelien-DRUON <aurelien.druon@epfedu.fr>
2024-02-19 16:18:30 +0100

    je déteste le style
    c'est horrible

--- resources/views/stats/read.blade.php
+++ resources/views/stats/read.blade.php
@@ -1,34 +1,38 @@
 @extends('layouts.layout')
-
 @section('content')
     @if ($role == '1')
-        <div>
+        <div class="w-4/5 mx-auto mt-10">
             @php
                 $global_average = 0;
                 $global_coefficient = 0;
             @endphp
-            @foreach ($tests as $test)
-                @php
-                    $average = 0;
-                    foreach ($test->marks as $mark) {
-                        $average += $mark->mark;
-                    }
-                    $average /= count($test->marks);
-                    $global_average += $average * $test->coefficient;
-                    $global_coefficient += $test->coefficient;
-                @endphp
-                <div class="flex flex-row fixed-width gap-4">
-                    <div>
-                        <p class="text-gray-800">{{ $test->title }}</p>
-                    </div>
-                    <div>
-                        <p class="text-gray-600">Coeff: {{ $test->coefficient }}</p>
-                    </div>
-                    <div>
-                        <p class="text-gray-600">Moyenne: {{ $average }}/20</p>
-                    </div>
-                </div>
-            @endforeach
+            <table class="w-full text-white bg-blue-500 divide-y divide-blue-600 table-auto">
+                <thead>
+                    <tr>
+                        <th class="px-4 py-2">Nom</th>
+                        <th class="px-4 py-2">Coefficient</th>
+                        <th class="px-4 py-2">Moyenne</th>
+                    </tr>
+                </thead>
+                @foreach ($tests as $test)
+                    @php
+                        $average = 0;
+                        foreach ($test->marks as $mark) {
+                            $average += $mark->mark;
+                        }
+                        $average /= count($test->marks);
+                        $global_average += $average * $test->coefficient;
+                        $global_coefficient += $test->coefficient;
+                    @endphp
+                    <tbody>
+                        <tr class="bg-blue-700 hover:bg-blue-800">
+                            <td class="px-4 py-2">{{ $test->title }}</td>
+                            <td class="px-4 py-2">{{ $test->coefficient }}</td>
+                            <td class="px-4 py-2">{{ $average }}</td>
+                        </tr>
+                    </tbody>
+                @endforeach
+            </table>
             @php
                 $global_average /= $global_coefficient;
                 $global_average = round($global_average, 2);
--- resources/views/tests/read.blade.php
+++ resources/views/tests/read.blade.php
@@ -2,38 +2,60 @@
 
 @section('content')
     @if ($role == '1')
-        <div>
-            @foreach ($tests as $test)
-                <a href="{{ route('marks.read', ['id' => $test->id]) }}">
-                    <div style="display: flex; flex-direction: row; gap:8px">
-                        <p>{{ $test->title }}</p>
-                        <p>Coeff: {{ $test->coefficient }}</p>
-                    </div>
-                </a>
-            @endforeach
+        <div class="w-4/5 mx-auto mt-10">
+            <table class="w-full text-white bg-blue-500 divide-y divide-blue-600 table-auto">
+                <thead>
+                    <tr>
+                        <th>Test</th>
+                        <th>Coefficient</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($tests as $test)
+                        <tr>
+                            <td>
+                                <a href="{{ route('marks.read', ['id' => $test->id]) }}">{{ $test->title }}
+                                </a>
+                            </td>
+                            <td>{{ $test->coefficient }}</td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
         </div>
         <a href="{{ route('tests.create') }}">
             <button>Nouveau contrôle</button>
         </a>
     @elseif ($role == '2')
-        @php
-            $average = 0;
-            $denominator = 0;
-            foreach ($marks as $mark) {
-                $average += $mark->mark * $mark->test->coefficient;
-                $denominator += $mark->test->coefficient;
-            }
-            $average /= $denominator;
-            $average = round($average, 2);
-        @endphp
-        <div style="display: flex; flex-direction: column">
-            @foreach ($marks as $mark)
-                <div style="display: flex; flex-direction: row">
-                    <p>{{ $mark->test->title }}</p>
-                    <p>{{ $mark->mark }}/20</p>
-                    <p>{{ $mark->description }}</p>
-                </div>
-            @endforeach
+        <div class="w-4/5 mx-auto mt-10">
+            @php
+                $average = 0;
+                $denominator = 0;
+                foreach ($marks as $mark) {
+                    $average += $mark->mark * $mark->test->coefficient;
+                    $denominator += $mark->test->coefficient;
+                }
+                $average /= $denominator;
+                $average = round($average, 2);
+            @endphp
+            <table class="w-full text-white bg-blue-500 divide-y divide-blue-600 table-auto">
+                <thead>
+                    <tr>
+                        <th>Test</th>
+                        <th>Note</th>
+                        <th>Description</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($marks as $mark)
+                        <tr>
+                            <td>{{ $mark->test->title }}</td>
+                            <td>{{ $mark->mark }}/20</td>
+                            <td>{{ $mark->description }}</td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
             <p>Moyenne : {{ $average }}/20</p>
         </div>
     @endif
